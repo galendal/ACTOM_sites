41bb33e1fce02a40e6e29db17842e1d04bbe27c6
Guttorm Alendal <guttorm.alendal@uib.no>
2022-07-12 14:29:17 +0200

    220712

--- North_Sea/NS_velocity.py
+++ North_Sea/NS_velocity.py
@@ -41,38 +41,77 @@ def find_nearest(xd, pos=[60.645556,3.726389]): # Default to Troll
     return [xloc,yloc]
 
 
+
+def crop_depth(inxarr):
+    return inxarr.isel(depth=(inxarr.depth<=inxarr.h).argmin(dim='depth')-1)
+
+# def find_mask(inxarr, pos=np.array([60.645556,3.726389]), span=np.array([0.15,0.15])):
+#     min_latlon=pos-span
+#     max_latlon=pos+span
+#     mask_lat = (inxarr.lat >= min_latlon[0]) & (inxarr.lat <= max_latlon[0])
+#     mask_lon = (inxarr.lon >= min_latlon[1]) & (inxarr.lon <= max_latlon[1])
+#     mask=mask_lon & mask_lat
+#     return mask
+
+# def cropped_data(inxarr, mask=None):
+#     if not mask.any():
+#         print('Maske empty, exiting')
+#         out=[]
+#     else:
+#         cropped_out = inxarr.where(mask, drop=True)
+#         out=cropped_out.isel(depth=(cropped_out.depth<=cropped_out.h).argmin(dim='depth')-1)
+#     return out
+    
+def cropped_data(inxarr, pos=[60.645556,3.726389],span=[4,4]):
+    ii,jj=find_nearest(inxarr, pos)
+    out = inxarr.isel(X=np.arange(jj-span[0],jj+span[0]+1),Y=np.arange(ii-span[1],ii+span[1]+1))
+    out=crop_depth(out)
+    return out
+
+def add_utm_coords(inxarr):
+    easting,northing, zone, letter=utm.from_latlon(inxarr.lat.values,inxarr.lon.values)
+    out=inxarr.copy()
+    out=out.assign_coords(utm_E=(('Y','X'),easting))
+    out=out.assign_coords(utm_N=(('Y','X'),easting))
+    out.attrs['UTM zone']=zone
+    out.attrs['UTM letter']=letter
+    return out
+
+def read_one(filenm,pos=np.array([60.645556,3.726389]), span=[4,4] ):
+    xd= xr.open_dataset(filenm)
+    
+    tmp=xd[['h','u_eastward','v_northward']]
+    
+    cropped=cropped_data(tmp, pos, span)
+    xd.close()
+    return cropped
+
 #%%
 troll_lat=60.645556
 troll_lon=3.726389
-latspan=1
-lonspan=0.5
+latspan=0.15
+lonspan=0.15
 
 
 # %%
-def read_one(filenm, ):
-    xd= xr.open_dataset('https://thredds.met.no/thredds/dodsC/fou-hi/norkyst800m-1h/NorKyst-800m_ZDEPTHS_his.an.2018120500.nc')
-    xd.close()
+
 
 #%%
-xd= xr.open_dataset('https://thredds.met.no/thredds/dodsC/fou-hi/norkyst800m-1h/NorKyst-800m_ZDEPTHS_his.an.2018120500.nc')
+xd = read_one('https://thredds.met.no/thredds/dodsC/fou-hi/norkyst800m-1h/NorKyst-800m_ZDEPTHS_his.an.2018120500.nc')
 
-tmp=xd[['h','u_eastward','v_northward']]
-tmp
-xd.close()
-#%%
-min_lon=troll_lon-lonspan
-max_lon=troll_lon+lonspan
-
-min_lat=troll_lat-latspan
-max_lat=troll_lat+latspan
-
-# %%
-mask_lon = (xd.lon >= min_lon) & (xd.lon <= max_lon)
-mask_lat = (xd.lat >= min_lat) & (xd.lat <= max_lat)
+xd
 
 #%%
-cropped_xd = xd.where(mask_lon & mask_lat, drop=True)
+filestem='https://thredds.met.no/thredds/dodsC/fou-hi/norkyst800m-1h/NorKyst-800m_ZDEPTHS_his.an.202205'
+filetail='00.nc'
+data=[]
+for mnt in np.arange(2):
+    filenm=filestem + str(mnt+1).zfill(2)+filetail
+    print('Reading: ', filenm)
+    data.append(read_one(filenm))
+
+#data=xr.concat(data, dim='time')
 # %%
 
-cropped_tmp = tmp.where(mask_lon & mask_lat, drop=True)
-cropped_tmp.h.plot.contourf(x='lon',y='lat')
+all_data.to_netcdf('vel_may.nc')
+# %%
