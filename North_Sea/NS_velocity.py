cf0f81220a6e9f43a1dd5faca6f80c68fcb3ef6e
galendal <guttorm.alendal@uib.no>
2022-07-12 15:35:15 +0200

    made script of NS_velocity.py, can run from command line

--- North_Sea/NS_velocity.py
+++ North_Sea/NS_velocity.py
@@ -3,14 +3,16 @@ from netCDF4 import Dataset,num2date
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime, timezone
-import time
+#import time
 import xarray as xr
 import utm
-import openpyxl
+#import openpyxl
 import pandas as pd
-import json
+#import json
 import re
 from scipy import spatial
+from calendar import monthrange
+import sys, getopt
 
 #%%
 def dms2dd(degrees, minutes, seconds, direction):
@@ -43,7 +45,10 @@ def find_nearest(xd, pos=[60.645556,3.726389]): # Default to Troll
 
 
 def crop_depth(inxarr):
-    return inxarr.isel(depth=(inxarr.depth<=inxarr.h).argmin(dim='depth')-1)
+    tmpu=(~np.isnan(inxarr.u_eastward)).cumsum(dim='depth').argmax(dim='depth')
+    tmpv=(~np.isnan(inxarr.v_northward)).cumsum(dim='depth').argmax(dim='depth')
+    tmp=np.minimum(tmpu,tmpv)
+    return inxarr.isel(depth=tmp)
 
 # def find_mask(inxarr, pos=np.array([60.645556,3.726389]), span=np.array([0.15,0.15])):
 #     min_latlon=pos-span
@@ -65,14 +70,14 @@ def crop_depth(inxarr):
 def cropped_data(inxarr, pos=[60.645556,3.726389],span=[4,4]):
     ii,jj=find_nearest(inxarr, pos)
     out = inxarr.isel(X=np.arange(jj-span[0],jj+span[0]+1),Y=np.arange(ii-span[1],ii+span[1]+1))
-    #out=crop_depth(out)
+    out=crop_depth(out)
     return out
 
 def add_utm_coords(inxarr):
-    easting,northing, zone, letter=utm.from_latlon(inxarr.lat.values,inxarr.lon.values)
+    easting,northing, zone, letter=utm.from_latlon(inxarr.lat.values,inxarr.lon.values, force_zone_number=31)
     out=inxarr.copy()
     out=out.assign_coords(utm_E=(('Y','X'),easting))
-    out=out.assign_coords(utm_N=(('Y','X'),easting))
+    out=out.assign_coords(utm_N=(('Y','X'),northing))
     out.attrs['UTM zone']=zone
     out.attrs['UTM letter']=letter
     return out
@@ -86,33 +91,74 @@ def read_one(filenm,pos=np.array([60.645556,3.726389]), span=[4,4] ):
     xd.close()
     return cropped
 
-#%%
-troll_lat=60.645556
-troll_lon=3.726389
-latspan=0.15
-lonspan=0.15
-
-
-# %%
-
 
 #%%
-xd = read_one('https://thredds.met.no/thredds/dodsC/fou-hi/norkyst800m-1h/NorKyst-800m_ZDEPTHS_his.an.2018120500.nc')
+def read_month(year=2022, month=5,pos=[60.7736192761523, 4.053348084930488],span=[10,10], save=False, verbose=True ):
+    filestem='https://thredds.met.no/thredds/dodsC/fou-hi/norkyst800m-1h/NorKyst-800m_ZDEPTHS_his.an.'
+    filetail='00.nc'
+    
+    data=[]
+    for day in np.arange(monthrange(year,month)[1]):
+        filenm=filestem + str(year) + str(month).zfill(2) + str(day+1).zfill(2)+filetail
+        if verbose:
+            print('Reading: ', filenm)
+        data.append(read_one(filenm,pos, span))
+    if verbose:
+        print('Concating the data')
+    data=xr.concat(data, dim='time')
+    if save:
+        outfile= 'NS_vel_'+str(year)+str(month).zfill(2) + '.nc'
+        if verbose:
+            print('Writing: '+outfile)
+        data.to_netcdf(outfile)
+    return data
 
-xd
-
-#%%
-filestem='https://thredds.met.no/thredds/dodsC/fou-hi/norkyst800m-1h/NorKyst-800m_ZDEPTHS_his.an.202205'
-filetail='00.nc'
-data=[]
-for mnt in np.arange(2):
-    filenm=filestem + str(mnt+1).zfill(2)+filetail
-    print('Reading: ', filenm)
-    data.append(read_one(filenm))
-
-#data=xr.concat(data, dim='time')
 # %%
+def main(argv):
+    year = 2022
+    month = 5
+    spanx=10
+    spany=10
+    lat=60.7736192761523
+    lon= 4.05334808493048
+    verbose=False
+    try:
+        opts, args = getopt.getopt(argv,"vhy:m:t:n:x:y:",["year=","month=","lat=","lon=", "spanx=","spany="])
+    except getopt.GetoptError:
+        print('test.py -y <year> -m <month> -t <lat> -n <lon> -x <span x> -y <span y>')
+        print('or')
+        print('test.py --year=<year> --month=<month> --lat=<lat> --lon=<lon> -x <span x> -y <span y>')
+        sys.exit(2)
+    
+    for opt, arg in opts:
+        if opt == '-h':
+            print('test.py -y <year> -m <month> -t <lat> -n <lon> -x <span x> -y <span y>')
+            print('or')
+            print('test.py --year=<year> --month=<month> --lat=<lat> --lon=<lon> -x <span x> -y <span y>')
+            sys.exit()
+        elif opt == '-v':
+            verbose = True
+        elif opt in ("-y", "--year"):
+            year = int(arg)
+        elif opt in ("-m", "--month"):
+            month = int(arg)
+        elif opt in ("-t","--lat"):
+            lat = float(arg)
+        elif opt in ("-n", "--lon"):
+            lon = float(arg)
+        elif opt in ("-x", "--spanx"):
+            spanx = int(arg)
+        elif opt in ("-y", "--spany"):
+            spany = int(arg)
+        
+    pos=[lat,lon]
+    span=[spanx,spany]
+    
+    #print('read_month(year='+ str(year) + "month="+ str(month) + ",span=" + str(span) + ", pos= "+ pos+ ", save=True)" )
+    read_month(year= year, month=month, span=span, pos= pos, save=True, verbose=verbose)    
 
-all_data.to_netcdf('vel_may.nc')
+if __name__ == '__main__':
+    main(sys.argv[1:])
+    #read_month(save=True)
+
 # %%
-tt=(~np.isnan(a)).cumsum(1).argmax(1)
